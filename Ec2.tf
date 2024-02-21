56aa1ea9241c2d0408376e035cf2ffc02daf48a0
sachhy <40334959+sachhy@users.noreply.github.com>
2024-02-21 19:12:31 +0100

    Update Ec2.tf

--- Ec2.tf
+++ Ec2.tf
@@ -1,11 +1,1 @@
-provider "aws" {
-  region = "ap-south-1"
-}
 
-resource "aws_instance" "Terraform_Created_Server" {
-  ami           = "ami-05a5bb48beb785bf1"
-  instance_type = "t2.micro"
-  key_name     = "Aws_Devops"
-}
-
-resource "" "" {}
